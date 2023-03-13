1f341e3d2e95c57ea5336d146390fdd37cf46fbe
Manya19112003 <122426697+Manya19112003@users.noreply.github.com>
2023-03-13 10:18:29 +0530

    Create program3.c

---
+++ program3.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter value of a and b");
+    scanf("%d %d",&a,&b);
+    if(a==b){
+        printf("they are equal");
+    }else{
+        printf("they are not equal");
+    }return 0;
+}
